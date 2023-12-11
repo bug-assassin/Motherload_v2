165f0552b90f456f45a052720cbd4999ce6bc4c2
bug-assassin <7788433+bug-assassin@users.noreply.github.com>
2023-12-10 23:18:29 -0500

    Working with mining to center of block logic

--- GenerateGround.gd
+++ GenerateGround.gd
@@ -1,19 +1,48 @@
 extends TileMap
+class_name TileManager
 
-
-const TILE_SKY = Vector2i(0, 0)
-const TILE_DIRT = Vector2i(0, 4)
+const TILE_DIRT = Vector2i(0, 0)
+const TILE_SKY = Vector2i(0, 4)
 const TILE_STONE = Vector2i(10, 4)
+const TILE_GOLD = Vector2i(5, 9)
 const ATLAS_NO = 1
+
+var layers_3_20 = [
+	[0.9, TILE_STONE],
+	[0.05, TILE_DIRT],
+	[0.05, TILE_GOLD]
+]
+
+func get_random_tile(distribution):
+	var randValue = randf()
+	var cumProb = 0
+	for item in distribution:
+		var prob = item[0]
+		var tile = item[1]
+		cumProb += prob
+		if randValue <= cumProb:
+			return tile
+	
+	assert(false)
 
 func _ready():
 	for x in range(20):
 		for y in range(7):
 			var tile = TILE_SKY if y < 2 else TILE_DIRT
-			if y == 3:
-				tile = TILE_STONE
+			if y >= 3 and y <= 20:
+				tile = get_random_tile(layers_3_20)
 			set_cell(0, Vector2i(x, y), ATLAS_NO, tile)
+			
 	print("finished")
-	
+
+func delete_tile(colliderId: RID):#globalCoord: Vector2):
+	var coords = get_coords_for_body_rid(colliderId)
+	set_cell(0, coords, ATLAS_NO, TILE_DIRT)
+
+func get_tile_origin(colliderId: RID): 
+	return to_global(map_to_local(get_coords_for_body_rid(colliderId)))
+
+func get_tile_hardness(colliderId: RID) -> int:
+	return get_coords_for_body_rid(colliderId)[1]
 func _process(delta):
 	pass
--- Player.gd
+++ Player.gd
@@ -4,6 +4,8 @@ var GRAVITY= 9.81
 var mass = 10
 @export var GravityOn: bool = true
 @export var damping = 0.01
+@export var DEBUG: bool = false
+@onready var tilemap: TileManager = get_node("/root/Node2D/TileMap")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print("Start")
@@ -13,20 +15,66 @@ func _process(delta):
 	if Input.is_action_just_pressed("toggleGravity"):
 		GravityOn = !GravityOn
 		print("Gravity changed")
-		
+
+var collisionLoc = []
+func _draw():
+	for col in collisionLoc:
+		draw_rect(Rect2(to_local(col), Vector2(5, 5)), Color.GREEN)
+
+var handlingDrill = false
+var collisionRID = null
+var collisionTime = 0
+var collisionDur = 1
+var originalPos = null
 func _physics_process(delta):
+	if handlingDrill:
+		collisionTime += delta
+		var percent = collisionTime / collisionDur * 100
+		position = lerp(originalPos, tilemap.get_tile_origin(collisionRID), percent / 100)
+		if percent >= 100:
+			collisionTime = 0
+			handlingDrill = false
+			tilemap.delete_tile(collisionRID)
+			$CollisionShape2D.disabled = false
+		return
 	var force = 15;
+	var action_pressed = false
 	if Input.is_action_pressed("up"):
 		velocity.y -= 20 * delta
+		action_pressed = true
 	elif Input.is_action_pressed("down"):
+		action_pressed = true
 		pass
 	elif Input.is_action_pressed("left"):
 		velocity.x += -force * delta
+		action_pressed = true
 	elif Input.is_action_pressed("right"):
 		velocity.x += force * delta
+		action_pressed = true
+		
 	if GravityOn:
 		velocity.y += GRAVITY*delta
 	velocity.x *= (1-damping)
+	
 	var collision = move_and_collide(velocity)
 	if collision:
 		velocity = velocity.slide(collision.get_normal())
+		if DEBUG:
+			collisionLoc.append(collision.get_position())
+			self.queue_redraw()
+	if collision and action_pressed:
+		var shouldDel = false
+		if collision.get_position() and Input.is_action_pressed("down") and collision.get_angle() == 0:
+			shouldDel = true
+		elif collision.get_position() and Input.is_action_pressed("left") and collision.get_angle() != 0:
+			print("Left collision: ", collision.get_angle())
+			shouldDel = true
+		elif collision.get_position() and Input.is_action_pressed("right") and collision.get_angle() != 0:
+			shouldDel = true
+			print("Right collision: ", collision.get_angle())
+		if shouldDel:
+			$CollisionShape2D.disabled = true
+			collisionRID = collision.get_collider_rid()
+			originalPos = position
+			collisionDur = tilemap.get_tile_hardness(collisionRID) / 6.0
+			handlingDrill = true
